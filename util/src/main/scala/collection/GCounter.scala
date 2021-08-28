133b99c81ee78113e169b16be2e7d3e80ba7d82c
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2021-08-28 17:27:31 +0800

    use monods for GCounter implementation

--- util/src/main/scala/collection/GCounter.scala
+++ util/src/main/scala/collection/GCounter.scala
@@ -1,19 +1,48 @@
 package net.zhenglai
 package collection
 
+import cats.kernel.CommutativeMonoid
+import cats.syntax.foldable._
+import cats.syntax.semigroup._
+
 // commutative replicated data types aka. CRDT for eventual consistency
 //  - key ops: merging 2 data instances with a result that captures all the information in both instances
 
 // distributed increment-only counter
 //  - e.g. count number of visitors to web sites
-final case class GCounter(counters: Map[String, Int]) {
-  def increment(machine: String, amount: Int): GCounter = {
-    val v = amount + counters.getOrElse(machine, 0)
+final case class GCounter[A](counters: Map[String, A] = Map.empty[String, A]) {
+  def increment(machine: String, amount: A)(implicit
+      ev: CommutativeMonoid[A]
+  ): GCounter[A] = {
+    val v = amount |+| counters.getOrElse(machine, ev.empty)
     GCounter(counters + (machine → v))
   }
-  def merge(that: GCounter): GCounter =
-    GCounter(this.counters ++ that.counters.map {
-      case (k, v) ⇒ k -> (v max this.counters.getOrElse(k, 0))
-    })
-  def total: Int = counters.values.sum
+  def merge(that: GCounter[A])(implicit
+      ev: BoundedSemiLattice[A]
+  ): GCounter[A] =
+    GCounter(this.counters ++ that.counters)
+  def total(implicit
+      ev: CommutativeMonoid[A]
+  ): A = counters.values.toList.combineAll
+}
+
+// commutative and idempotent
+trait BoundedSemiLattice[A] extends CommutativeMonoid[A] {
+  def combine(x: A, y: A): A
+  def empty: A
+}
+
+object BoundedSemiLattice {
+  // TODO: how to model non-negative numbers?
+  implicit val intInstance: BoundedSemiLattice[Int] =
+    new BoundedSemiLattice[Int] {
+      override def combine(x: Int, y: Int): Int = x max y
+      override def empty: Int = 0
+    }
+
+  implicit def setInstance[A](): BoundedSemiLattice[Set[A]] =
+    new BoundedSemiLattice[Set[A]] {
+      override def combine(x: Set[A], y: Set[A]): Set[A] = x union y
+      override def empty: Set[A] = Set.empty[A]
+    }
 }
---
+++ util/src/test/scala/collection/GCounterTest.scala
@@ -0,0 +1,19 @@
+package net.zhenglai
+package collection
+
+import org.scalatest.FunSuite
+
+class GCounterTest extends FunSuite {
+
+  test("testMerge") {
+    val gc1 = new GCounter[Int]()
+      .increment("m1", 1)
+      .increment("m2", 2)
+    val gc2 = new GCounter[Int]()
+      .increment("m1", 3)
+    val gc = gc1.merge(gc2)
+    println(gc)
+    println(gc.total)
+  }
+
+}
