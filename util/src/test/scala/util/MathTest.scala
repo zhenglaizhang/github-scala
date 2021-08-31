41ad5789d02e0f8510fcb5203745e070e5af5cd3
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2021-08-31 08:31:33 +0800

    fix MathTest tests

--- util/src/test/scala/util/MathTest.scala
+++ util/src/test/scala/util/MathTest.scala
@@ -9,15 +9,15 @@ class MathTest extends FunSuite {
 
   test("testSquareRoot") {
     squareRoot.isDefinedAt(-1)
-    squareRoot(100)
-    squareRoot.lift(-1)
-    println(List(-1, 2, 3, 4, 0, -3).collect(squareRoot))
+    assert(squareRoot(625) === 25)
+    assert(squareRoot.lift(625) === Some(25))
+    assert(List(-1, 9, 4, 0, -3).collect(squareRoot) === Seq(3, 2, 0))
 
     val square: PartialFunction[Int, Double] = {
       case a if a < 0 ⇒ java.lang.Math.pow(a, 2)
     }
     val f = squareRoot.orElse(square)
-    println(f.isDefinedAt(-1))
+    assert(f.isDefinedAt(-1) === true)
   }
 
 }
