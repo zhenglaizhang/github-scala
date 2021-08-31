9a5f387da541a01bbfaa3fbb0c215c32346e6409
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2021-08-31 19:32:23 +0800

    comment about RTB model

--- rtb/core/src/main/scala/net/zhenglai/adx/model/AdRequest.scala
+++ rtb/core/src/main/scala/net/zhenglai/adx/model/AdRequest.scala
@@ -5,14 +5,20 @@ import cats.data.NonEmptyList
 final case class AdRequest()
 final case class AdResponse()
 
+/**
+  * Top level object for RTB bidding request
+  */
 final case class BidRequest(
     site: Site,
     content: Content,
     user: Option[User],
     device: Option[Device],
+    test: Boolean =
+      false, // 0 = live mode, 1 = test mode (auctions are not billable), default 0,
+    at: Int = AuctionType.SecondPricePlus,
     regs: Seq[Regs],
     source: Seq[Source],
-    impressions: NonEmptyList[Impression] // only this is technically required
+    imp: NonEmptyList[Imp] // only this is technically required
 )
 final case class BidResponse(
     bid: Bid,
@@ -46,7 +52,10 @@ case class Bid()
 
 case class Creative()
 
-case class Impression(
+/**
+  * Container for the description of a specific impression; at least 1 per request
+  * */
+case class Imp(
     native: Option[Native],
     banner: Option[Banner],
     video: Option[Video],
@@ -55,9 +64,16 @@ case class Impression(
     pmp: Option[Pmp]
 )
 
+/**
+  * A quantifiable often historical data point about an impression
+  */
 case class Metric()
 case class Native()
 case class Banner(formats: Seq[Format])
+
+/**
+  * An allowed size of a banner
+  */
 case class Format()
 
 /**
@@ -76,8 +92,31 @@ case class Audio(
 case class Pmp(deal: Seq[Deal])
 case class Deal()
 
+/**
+  * Regulatory conditions in effect for all impressions in this bid request
+  */
 case class Regs()
+
+/**
+  * Request source details on post-auction decisioning (e.g. header bidding)
+  */
 case class Source()
 
 case class DistributionChannel(
 )
+
+/**
+  * Details of the application calling for the impression
+  */
+case class App()
+
+/**
+  * Entity that controls the content of and distributes the site or app
+  */
+case class Publisher()
+
+// todo use enum
+object AuctionType {
+  val FirstPrice = 1
+  val SecondPricePlus = 2
+}
