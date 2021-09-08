c758042c9cc455e72305bcaf01fdbb865ea3d911
Zhenglai Zhang <darinzh@microsoft.com>
2021-09-08 23:26:49 +0800

    Function (#38)
    * partial functions vs Functions returning Options
    
    * list & vector
    
    * map
    
    * set

---
+++ scala3/play/src/main/scala/basic/Collec.scala
@@ -0,0 +1,4 @@
+package net.zhenglai
+package basic
+
+object Collec {}
---
+++ scala3/play/src/main/scala/basic/DsList.scala
@@ -0,0 +1,32 @@
+package net.zhenglai
+package basic
+
+@main def list() = {
+  assert(List.empty[Nothing] == Nil)
+}
+
+@main def vector() = {
+  val v1 = Vector.empty :+ "hello" :+ "world"
+  println(v1)
+  val v2 = "hello" +: "world" +: Vector.empty
+  println(v2)
+  v2.head
+  v2.tail
+
+  // inefficient, a copy of the collection has to be constructed if the Seq is not a vector
+  Seq(1, 2, 3).toVector
+}
+
+@main
+def map() = {
+  val m = Map("one" -> 1, "two" -> 2)
+  m + ("three" -> 3)
+  m ++ Seq("four" -> 4)
+}
+
+@main
+def set() = {
+  val s = Set("one", "two", "one")
+  s + "three"
+  s ++ Seq("three", "four")
+}
