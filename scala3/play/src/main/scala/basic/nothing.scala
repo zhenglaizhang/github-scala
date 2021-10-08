9ae603766a9ac3ef44f85243867c78ed92113f35
Zhenglai Zhang <darinzh@microsoft.com>
2021-10-08 21:26:32 +0800

    nothing-null (#57)

--- scala3/play/src/main/scala/basic/nothing.scala
+++ scala3/play/src/main/scala/basic/nothing.scala
@@ -3,6 +3,36 @@ package basic
 
 import scala.Nothing
 import scala.NotImplementedError
+import scala.Option
+
+// null, Null, None, Nil, Unit and Nothing
+//  - the null reference belongs to its own distinct type, which is called Null
+//  - the Null type has no methods, no fields, cannot be extended or instantiated
+//  - Null extends all reference types, Null is a proper subtype of all reference types.
+//  - Using null incentivizes us to write imperative, Java-style, defensive and error-prone code
+
+//  - Nil is the empty implementation of a List
+//  - Instead of null, we commonly use Options,
+//    which are data structures meant to represent the presence or absence of a value
+
+//  - the equivalent of “void” in other languages is Unit in Scala
+
+//  - Nothing is the type of no value at all.
+//  - Nothing can’t be instantiated and has no values of that type.
+//  - Nothing truly means nothing: not even null, not Unit, not None, nothing at all.
+//  - The only expressions that return Nothing are throwing exceptions
+//  - Nothing is a proper subtype for all possible types in Scala.
+//  - Much like Null, Nothing is treated in a special way by the compiler
+object nulla {
+  val novalue: String = null
+  var theNullRef: Null = null
+  val noList: List[Int] = theNullRef
+
+  val emptyList: List[Int] = Nil
+  val emptyListLength: Int = Nil.length
+
+  val anAbsentValue: Option[String] = Option(novalue)
+}
 
 // Scala is fully object-oriented, but the type hierarchy is not so standard.
 object nothing {
